b4bb73d689dcd7593725a5abae1b3c70c3eacdd6
Isuru Fernando <isuruf@gmail.com>
2020-07-26 10:07:15 -0500

    Add a spinner for travis

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,35 @@
 
 set -e
 
+function start_spinner {
+    if [ -n "$SPINNER_PID" ]; then
+        return
+    fi
+
+    >&2 echo "Building libraries..."
+    # Start a process that runs as a keep-alive
+    # to avoid travis quitting if there is no output
+    (while true; do
+        sleep 60
+        >&2 echo "Still building..."
+    done) &
+    SPINNER_PID=$!
+    disown
+}
+
+function stop_spinner {
+    if [ ! -n "$SPINNER_PID" ]; then
+        return
+    fi
+
+    kill $SPINNER_PID
+    unset SPINNER_PID
+
+    >&2 echo "Building libraries finished."
+}
+
+start_spinner
+
 mkdir -p build_conda
 cd build_conda
 
@@ -52,3 +81,4 @@ else
   sed "s#@CONDA_PREFIX@#$CONDA_PREFIX#g" $RECIPE_DIR/libgfortran.spec > $PREFIX/lib/libgfortran.spec
 fi
 
+stop_spinner
