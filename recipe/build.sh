b5960ec698e08bf2fe7bca446a2fc2b59aced292
Isuru Fernando <isuruf@gmail.com>
2020-09-20 14:34:03 -0500

    Add a libgfortran-devel output

--- recipe/build.sh
+++ recipe/build.sh
@@ -138,12 +138,15 @@ if [[ "$host_platform" == "$target_platform" ]]; then
     sed -i.bak "s@^\*lib.*@& -rpath $PREFIX/lib@" libgfortran.spec
     rm libgfortran.spec.bak
   popd
+
+  for file in libgfortran.spec libgomp.spec libgfortran.a libgomp.a; do
+    mv $PREFIX/lib/$file $PREFIX/lib/${TARGET}/${gfortran_version}/$file
+    ln -s $PREFIX/lib/${TARGET}/${gfortran_version}/$file $PREFIX/lib/$file
+  done
 else
   # The compiler is a cross compiler. Only make the compiler. No target libraries
   make all-gcc -j${CPU_COUNT}
   make install-gcc -j${CPU_COUNT}
-  cp $RECIPE_DIR/libgomp.spec $PREFIX/lib/gcc/${TARGET}/${gfortran_version}/libgomp.spec
-  sed "s#@CONDA_PREFIX@#$PREFIX#g" $RECIPE_DIR/libgfortran.spec > $PREFIX/lib/gcc/${TARGET}/${gfortran_version}/libgfortran.spec
 fi
 
 stop_spinner
