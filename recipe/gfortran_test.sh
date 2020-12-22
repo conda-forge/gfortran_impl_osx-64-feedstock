4acdd53a41bc5a31690e8e4f12c9210fe87956ee
Philippe Blain <levraiphilippeblain@gmail.com>
2020-12-22 08:32:26 -0500

    recipe: add OpenMP threadprivate test
    In bb2a5bd (Fix linking to libgcc_s, 2020-12-21), the libgfortran.spec
    file was fixed so that compiling code with 'threadprivate' OpenMP
    directives does not require linking with the internal libgcc_eh library.
    
    To prevent future regressions, add a test to the test script
    'gfortran_test.sh' that compiles and runs a Fortran program with OpenMP
    threadprivate directives. The program is taken from the OpenMP examples
    [1].
    
    Closes conda-forge/gfortran_impl_osx-64-feedstock#36
    
    [1] https://github.com/OpenMP/Examples/blob/master/sources/Example_threadprivate.5.f

--- recipe/gfortran_test.sh
+++ recipe/gfortran_test.sh
@@ -12,4 +12,8 @@ if [[ "$target_platform" == "$cross_target_platform" ]]; then
   "${PREFIX}/bin/${macos_machine}-gfortran" -O3 -fopenmp -ffast-math -o maths maths.f90 -v
   ./maths
   rm -f maths
+
+  "${PREFIX}/bin/${macos_machine}-gfortran" -fopenmp -o omp-threadprivate omp-threadprivate.f90 -v
+  ./omp-threadprivate
+  rm -f omp-threadprivate
 fi
