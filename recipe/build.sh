d162ef80cb2973b69623613f6bf6155aabd5336a
Isuru Fernando <isuruf@gmail.com>
2021-07-27 14:38:28 -0700

    make cc1 executable

--- recipe/build.sh
+++ recipe/build.sh
@@ -186,3 +186,4 @@ ls -al $PREFIX/lib
 
 mv ${PREFIX}/libexec/gcc/${TARGET}/${gfortran_version}/cc1 ${PREFIX}/libexec/gcc/${TARGET}/${gfortran_version}/cc1.bin
 sed "s#@PATH@#${PREFIX}/libexec/gcc/${TARGET}/${gfortran_version}#g" ${RECIPE_DIR}/cc1 > ${PREFIX}/libexec/gcc/${TARGET}/${gfortran_version}/cc1
+chmod +x ${PREFIX}/libexec/gcc/${TARGET}/${gfortran_version}/cc1
