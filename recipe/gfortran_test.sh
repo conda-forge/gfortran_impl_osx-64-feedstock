52fdaf464c53e39ea4bda00608237786f46f22ed
Kai Tietz <ktietz@anaconda.com>
2022-05-20 08:21:18 +0200

    remove usage of cross_target_platform

--- recipe/gfortran_test.sh
+++ recipe/gfortran_test.sh
@@ -9,19 +9,17 @@ if [[ ${target_platform} == osx-64 ]]; then
   export MACOSX_DEPLOYMENT_TARGET=10.9
 fi
 
-if [[ "$target_platform" == "$cross_target_platform" ]]; then
-  "${PREFIX}/bin/${macos_machine}-gfortran" -o hello hello.f90 -v -L$CONDA_BUILD_SYSROOT/usr/lib -L$CONDA_BUILD_SYSROOT/usr/lib/system
-  ./hello
-  rm -f hello
+"${PREFIX}/bin/${macos_machine}-gfortran" -o hello hello.f90 -v -L$CONDA_BUILD_SYSROOT/usr/lib -L$CONDA_BUILD_SYSROOT/usr/lib/system
+./hello
+rm -f hello
 
-  "${PREFIX}/bin/${macos_machine}-gfortran" -O3 -fopenmp -ffast-math -o maths maths.f90 -v -L$CONDA_BUILD_SYSROOT/usr/lib -L$CONDA_BUILD_SYSROOT/usr/lib/system
-  ./maths
-  rm -f maths
+"${PREFIX}/bin/${macos_machine}-gfortran" -O3 -fopenmp -ffast-math -o maths maths.f90 -v -L$CONDA_BUILD_SYSROOT/usr/lib -L$CONDA_BUILD_SYSROOT/usr/lib/system
+./maths
+rm -f maths
 
-  "${PREFIX}/bin/${macos_machine}-gfortran" -fopenmp -o omp-threadprivate omp-threadprivate.f90 -v -L$CONDA_BUILD_SYSROOT/usr/lib -L$CONDA_BUILD_SYSROOT/usr/lib/system
-  ./omp-threadprivate
-  rm -f omp-threadprivate
+"${PREFIX}/bin/${macos_machine}-gfortran" -fopenmp -o omp-threadprivate omp-threadprivate.f90 -v -L$CONDA_BUILD_SYSROOT/usr/lib -L$CONDA_BUILD_SYSROOT/usr/lib/system
+./omp-threadprivate
+rm -f omp-threadprivate
 
-  ${macos_machine}-gfortran -v
-  ${macos_machine}-gfortran -E -dM - </dev/null
-fi
+${macos_machine}-gfortran -v
+${macos_machine}-gfortran -E -dM - </dev/null
