4141bfb9dc62049441233526ce66b96af8090f80
Isuru Fernando <isuruf@gmail.com>
2023-02-17 20:10:10 -0600

    Update gfortran_test.sh

--- recipe/gfortran_test.sh
+++ recipe/gfortran_test.sh
@@ -2,7 +2,8 @@
 
 set -xe
 
-${PREFIX}/bin/${macos_machine}-gfortran -v
+# TODO: figure out why the following line does not work on CI
+# ${PREFIX}/bin/${macos_machine}-gfortran -v
 
 if [[ "$target_platform" == "$cross_target_platform" ]]; then
   "${PREFIX}/bin/${macos_machine}-gfortran" -o hello hello.f90 -v
